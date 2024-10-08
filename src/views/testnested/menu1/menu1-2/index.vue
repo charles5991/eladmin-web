9beff41180430731e8023ddd48e63184a5d03c2c
Charles Chan <charleschan5991@gmail.com>
2024-10-08 19:23:27 +0800

    新增菜单页面
    - 更改测试端接口
    - 新增主菜单，二级菜单， 三级菜单页面

---
+++ src/views/testnested/menu1/menu1-2/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app-container">
+    <el-alert :closable="false" title="测试三级菜单2" type="success" />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.app-container {
+  padding: 30px;
+  background-color: #133466;
+}
+</style>
