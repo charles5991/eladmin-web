ee1e6c76c3a85c70b0b9b457db1ebe39e0ceb18d
Charles Chan <charleschan5991@gmail.com>
2024-10-09 20:04:22 +0800

    Update index.vue

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -3,14 +3,15 @@
     <!--工具栏-->
     <div class="head-container">
       <div style="margin-bottom: 20px;">      
-        <el-descriptions title="垂直带边框列表" direction="vertical" :column="4" border>
+        <el-descriptions title="列表11" direction="vertical" :column="4" border>
           <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
           <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
           <el-descriptions-item label="居住地" :span="2">苏州市</el-descriptions-item>
           <el-descriptions-item label="备注">
             <el-tag size="small">学校</el-tag>
+            <el-tag size="small" color="pink" style="margin-left: 10px;">公园</el-tag>
           </el-descriptions-item>
-          <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+          <el-descriptions-item label="联系地址">地址</el-descriptions-item>
         </el-descriptions>
       </div>
 
